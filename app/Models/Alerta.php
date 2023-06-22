7895e9504a4610f99d9376383a3386bc2fd29b37
smonsal <smonsal@bancolombia.com.co>
2023-06-22 01:13:36 -0500

    primer commit - Se agregan todos los componentes de la App

---
+++ app/Models/Alerta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alerta extends Model
+{
+    use HasFactory;
+}
