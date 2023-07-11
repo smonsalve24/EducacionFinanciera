7896910caf7c2d474ff2739a7cb7abfb95dcae3a
smonsal <smonsal@bancolombia.com.co>
2023-07-11 18:59:55 -0500

    Creación del archivo Historico

--- app/Models/historico.php
+++ app/Models/historico.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class historico extends Model
+class Historico extends Model
 {
     use HasFactory;
 }
--- database/migrations/2023_06_24_221039_create_historicos_table.php
+++ database/migrations/2023_06_24_221039_create_historicos_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('historicos', function (Blueprint $table) {
             $table->id();
-            $table->string('descargas');
             $table->date('rango_fecha');
             $table->unsignedBigInteger('ingreso_id');
             $table->foreign('ingreso_id')->references('id')->on('ingresos')->onDelete('cascade');
